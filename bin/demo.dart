e677947e6fefc44d96cabf313f675c238104f78d
jyothymol999 <jyothymol12@gmail.com>
2022-09-22 10:14:15 -0700

    commit

---
+++ bin/demo.dart
@@ -0,0 +1,17 @@
+void main() {
+  //variable declaration
+  //syntax->datatype variablename='value';
+  String name='jyothy';
+  int age=24;
+  String email='jo@FG';
+  String college='mzc';
+  String course='mca';
+  double mark=88.777;
+  int phno=9996045623;
+  print('my name is:${name}');
+  print('i am :${age} years old');
+  print('my collge name is:${college}');
+  print('my course is:${course}');
+  print('my mark is:${mark}');
+  print('phno is:${phno}');
+}
