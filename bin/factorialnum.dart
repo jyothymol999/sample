0894e629e6b6ebe3289cce30e9bd0fbdea7b2328
jyothymol999 <jyothymol12@gmail.com>
2022-09-28 09:34:18 -0700

    commitz

--- bin/factorialnum.dart
+++ bin/factorialnum.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 void  main(){
+
+  int num,factorial=1;
+  print('enter a number');
+  num=int.parse(stdin.readLineSync()!);
+  for(int i=1;i<=num;i++){
+    factorial=factorial*i;
+  }
+  print('factorial of $num is $factorial');
+
+
 
 }
